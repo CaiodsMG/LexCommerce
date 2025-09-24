abd9bed82a41416711a9bfa1ff2cbefbf115a47e
CaiodsMG <caiocfn10@gmail.com>
2025-09-24 14:51:02 -0300

    tipo de saída com ResponseEntity e ResponseStatus

--- src/main/java/com/lex/LexCommerce/controllers/ProdutoController.java
+++ src/main/java/com/lex/LexCommerce/controllers/ProdutoController.java
@@ -1,11 +1,12 @@
 package com.lex.LexCommerce.controllers;
 
 import com.lex.LexCommerce.dto.ProdutoDTO;
-import com.lex.LexCommerce.entities.Produto;
 import com.lex.LexCommerce.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,17 +20,19 @@ public class ProdutoController {
     private ProdutoService service;
 
     @GetMapping("/{id}")
-    public ProdutoDTO findById(@PathVariable Long id){
+    public ResponseEntity<ProdutoDTO> findById(@PathVariable Long id){
         ProdutoDTO dto = service.findById(id);
-        return dto;
+        return ResponseEntity.ok(dto);
     }
 
     @GetMapping()
-    public Page<ProdutoDTO> findAll(Pageable pageable){
-        return service.findAll(pageable);
+    public ResponseEntity<Page<ProdutoDTO>> findAll(Pageable pageable){
+        Page<ProdutoDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
     }
 
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     public ProdutoDTO inserirProduto(@RequestBody ProdutoDTO produto){
         return service.inserirProduto(produto);
     }
