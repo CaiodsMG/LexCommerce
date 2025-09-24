031f87c8e58f9dfebb85ebebd38ec500045947f3
CaiodsMG <caiocfn10@gmail.com>
2025-09-24 14:30:42 -0300

    Busca paginada (Service / Controller)

--- src/main/java/com/lex/LexCommerce/controllers/ProdutoController.java
+++ src/main/java/com/lex/LexCommerce/controllers/ProdutoController.java
@@ -1,15 +1,16 @@
 package com.lex.LexCommerce.controllers;
 
 import com.lex.LexCommerce.dto.ProdutoDTO;
-import com.lex.LexCommerce.entities.Produto;
-import com.lex.LexCommerce.repositories.ProdutoRepository;
 import com.lex.LexCommerce.services.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +24,10 @@ public class ProdutoController {
     public ProdutoDTO findById(@PathVariable Long id){
         ProdutoDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping()
+    public Page<ProdutoDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
--- src/main/java/com/lex/LexCommerce/services/ProdutoService.java
+++ src/main/java/com/lex/LexCommerce/services/ProdutoService.java
@@ -4,9 +4,12 @@ import com.lex.LexCommerce.dto.ProdutoDTO;
 import com.lex.LexCommerce.entities.Produto;
 import com.lex.LexCommerce.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +24,11 @@ public class ProdutoService {
         Produto produto = result.get();
         ProdutoDTO dto = new ProdutoDTO(produto);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProdutoDTO> findAll(Pageable pageable){
+        Page<Produto> result = repository.findAll(pageable);
+        return result.map(x -> new ProdutoDTO(x));
     }
 }
